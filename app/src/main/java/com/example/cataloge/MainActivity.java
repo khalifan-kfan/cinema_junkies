ece9b56cddeb2ca328d70d2706280ff4819b24d9
raymond-256 <77051184+raymond-256@users.noreply.github.com>
2021-02-09 05:35:27 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/cataloge/MainActivity.java
+++ app/src/main/java/com/example/cataloge/MainActivity.java
@@ -20,11 +20,14 @@ public class MainActivity extends AppCompatActivity {
        navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+       // using naviigation component linked to the navigation graph
+       
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_history)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+       //so the bottom nativigation bar is linked to the navigation graph for the three fragments 
         NavigationUI.setupWithNavController(navView, navController);
 
 
